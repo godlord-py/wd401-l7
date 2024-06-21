4acf5dcd5648548ceb4d2e699b35b191636f288e
Aayus <godlord@AayushPathak>
2024-06-21 14:51:24 +0530

    deploy to netlify

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    pageLoadTimeout: 60000,
+    pageLoadTimeout: 6000000,
     baseUrl: 'https://6675341083621c4054373a4f--sportsnews301.netlify.app/', // Replace with your actual Netlify URL
     setupNodeEvents(on, config) {
       // implement node event listeners here
