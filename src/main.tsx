473b086ff48a6a1da895d863b050955294007e19
Aayus <godlord@AayushPathak>
2024-06-26 18:53:29 +0530

    minor issue fix(ignore M8)

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import 'rsuite/dist/rsuite.min.css'
-import "/home/godlord/capstone301/sportnewsapp/src/i18n.ts"
+
 ReactDOM.createRoot(document.getElementById('root')).render(  
        <App />
 )
